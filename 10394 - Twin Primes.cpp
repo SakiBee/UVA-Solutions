0897fc78b702be81dde8d5a6973e3db2278eafd4
Md. Sakib Uddin <63546044+SakiBee@users.noreply.github.com>
2022-08-31 00:41:49 +0600

    Update 10394 - Twin Primes.cpp

--- 10394 - Twin Primes.cpp
+++ 10394 - Twin Primes.cpp
@@ -1,12 +1,13 @@
-
-https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1335
-
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1335
 
 //** In the name of Allah, most Gracious, most Compassionate **//
 #include <bits/stdc++.h>
 using namespace std;
 #define     run           ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define     ll              long long
+#define     ull             unsigned long long
+#define     bll             __int128
+
 #define     gcd(a,b)        __gcd(a,b)
 #define     lcm(a,b)        (a/__gcd(a,b))*b
 #define     vll             vector<ll>
@@ -24,35 +25,36 @@ using namespace std;
 #define     forrv(i,b,e)    for(int i=b;i>=e;i--)
 #define     sp(a)           fixed<<setprecision(a)
 #define     endl            '\n'
-#define     yes             cout << "YES" << '\n'
-#define     no              cout << "NO" << '\n'
+#define     yes             cout << "Yes" << '\n'
+#define     no              cout << "No" << '\n'
 
 
+const ll N = 2e7 + 9;
+vector <ll> primes;
+bool prime[N];
+void sieve(){
+    prime[1] = true;
+    for(ll i = 2; i*i <= N-9; i++) if(!prime[i]){
+        for(ll j = i*i; j <= N-9; j+=i) prime[j] = true;}
+    for(ll i=2; i<=N-9; i++){if(!prime[i]) primes.pb(i);}
+}
 
-const ll mod = 1e9+7;
-const ll M = 6e6+7;
-
-const int N = 10000005; vector <ll> primes; bool prime[N];
-void sieve(){memset(prime,1,sizeof(prime)); primes.pb(2); for(ll i = 4; i<=N; i+=2) prime[i] = 0;
-     for(ll i = 3; i<=N; i+=2) if(prime[i]){primes.pb(i); for(ll j = i*i; j<=N; j+=i*2LL) prime[j] = 0;}}
-
+map<ll,pair<ll,ll>>mp;
 
 int main(){
     sieve();
-    ll n;
-    while(1){
-        cin >> n;
-        if(n == 0) break;
-        ll ans = 0, p = n;
-        for(ll i=0; i<primes.size() && primes[i]*primes[i]<=n; i++){
-            if(n%primes[i]==0){
-                while(n%primes[i] == 0){
-                    n/=primes[i];
-                }
-                ans++;
-            }
+    ll cnt = 1;
+    for(ll i=0; i<primes.size(); i++){
+        if(primes[i]+2 == primes[i+1]){
+            mp[cnt] = {primes[i],primes[i+1]};
+            cnt++;
         }
-        if(n>1) ans++;
-        cout << p << " : " << ans << endl;
+        if(cnt > 100009) break;
     }
+    ll n;
+    while (cin >> n){
+        auto u = mp[n];
+        cout << "(" << u.F << ", " << u.S << ")" << endl;
+    }
+    return 0;
 }
